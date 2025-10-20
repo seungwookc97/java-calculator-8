7f87c27629e937a08dfed0f6dae01a01faf6e1f4
seungwookc97 <sochoi0832@gmail.com>
2025-10-20 20:54:44 +0900

    feat(calculator): 커스텀 구분자 처리 기능 구현
    - "//"와 "\n" 사이의 문자를 커스텀 구분자로 인식
    - 커스텀 구분자로 숫자 분리 및 합산

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import java.util.regex.Pattern;
+
 class Calculator {
 
     public static int calculate(String input) {
@@ -7,8 +9,27 @@ class Calculator {
             return 0;
         }
 
-        String[] numbers = input.split(",|:");
-        return sum(numbers);
+        String operator = ",|:";
+        String numbers = input;
+
+        if (input.startsWith("//")) {
+            int newlineIndex = input.indexOf("\n");
+            if (newlineIndex == -1) {
+                newlineIndex = input.indexOf("\\n");
+                if (newlineIndex != -1) {
+                    String customDelimiter = input.substring(2, newlineIndex);
+                    operator = Pattern.quote(customDelimiter);
+                    numbers = input.substring(newlineIndex + 2);
+                }
+            } else {
+                String addOperator = input.substring(2, newlineIndex);
+                operator = Pattern.quote(addOperator);
+                numbers = input.substring(newlineIndex + 1);
+            }
+        }
+
+        String[] numberArray = numbers.split(operator);
+        return sum(numberArray);
     }
 
     private static int sum(String[] numbers) {
